dd9801ce0d389ea5cb14e78f40a6f15966546dd9
Iarley-01 <yarleyalves2016@gmail.com>
2023-11-24 00:13:18 -0300

    Protótipo adicionado

---
+++ README.md
@@ -0,0 +1,12 @@
+# Game Detona Ralph
+Desafio de projeto do bootcamp [Potência Tech iFood](https://www.dio.me/bootcamp/potencia-tech-ifood-desenvolvimento-de-jogos), da Digital Innovation One.
+
+![Detona Ralph Game Screenshot](/src/images/Screenshot_20231124-000344_Acode.jpg)
+
+## Ferramentas Usadas
+• Linguagens
+- HTML/CSS: Para estilização e estruturação do jogo
+- Javascript: para implementação da lógico do jogo
+
+## Como Jogar
+Clone este repositório ou dê um fork e execute o arquivo index.html em seu navegador
---
+++ src/scripts/engine.js
@@ -0,0 +1,100 @@
+const state = {
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+    lives: document.getElementById("#live-points"),
+    button: document.getElementById("#btn-continue"),
+  },
+  values: {
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 30,
+    livePoints: 3,
+  },
+  
+  actions:{
+    timerId: setInterval(randomSquare, 1000),
+    countDownTimerId: setInterval(countDown, 1000),
+  }
+};
+
+function resetGame() {
+  //Function to reinitialize the game
+  clearInterval(state.actions.countDownTimerId);
+  clearInterval(state.actions.timerId);
+ //Reinitialize the time and the score
+  state.values.currentTime = 30;
+  state.view.timeLeft.textContent = state.values.currentTime;
+
+  state.values.result = 0;
+  state.view.score.textContent = state.values.result;
+//Set the time interval to the enemy
+  state.actions.timerId = setInterval(randomSquare, 1000);
+  state.actions.countDownTimerId = setInterval(countDown, 1000);
+}
+
+
+function countDown() {
+  //This function reduces the time counter
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+  //Verifies if the time reached 0
+  if (state.values.currentTime <= 0) {
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+    alert("Game Over! O seu resultado foi: " + state.values.result);
+    resetGame();
+  }
+  
+}
+
+function playSound(audioName) {
+  //this function plays an audio in the game
+  let audio = new Audio(`./src/audios/${audioName}.m4a`);
+  audio.volume = 0.2;
+  audio.play();
+}
+
+function randomSquare() {
+  //this function controls the appearances of the enemy
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy");
+  });
+  
+  let randomNumber = Math.floor(Math.random() * 9);
+  let randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
+}
+
+/*function moveEnemy() {
+  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}*/
+
+
+function addListenerHitBox() {
+  //this function controls if the mouse clicked in a enemy or not
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+        playSound("hit");
+      }
+    })
+  })
+}
+
+function init() {
+  //Main function of the game
+  alert("Acerte o alvo para marcar pontos");
+  addListenerHitBox();
+
+  resetGame();
+}
+
+init();
